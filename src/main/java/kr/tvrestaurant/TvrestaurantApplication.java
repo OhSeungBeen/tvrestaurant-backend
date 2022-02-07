44073b66d515f24d886e96eca0b892458d1546f8
OhSeungBeen <seungbeenoh@gmail.com>
2022-02-07 14:40:44 +0900

    Add: project setting

---
+++ src/main/java/kr/tvrestaurant/TvrestaurantApplication.java
@@ -0,0 +1,15 @@
+package kr.tvrestaurant;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvrestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvrestaurantApplication.class, args);
+
+	}
+
+}
